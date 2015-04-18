3133382266bd9f32b4f6e870aa8f0596d8a4d141
Alan Berman <alan.berman.92@gmail.com>
2015-04-18 16:03:01 +0200

    Commit of modified driver.cpp that now supports all menu-esque operations

--- driver.cpp
+++ driver.cpp
@@ -14,6 +14,7 @@ int main(int argc, char * argv[])
 	string file_one,file_two;
 	//String used to parse args
 	string s;
+	int thresh_int;
 	for (int i=2;i<argc;i++)
 	{
 		//Identify the option chosen
@@ -28,6 +29,7 @@ int main(int argc, char * argv[])
 			//Extract the second file name from the args
 			file_two=string(argv[i]);
 			//add
+			break;
 		}
 		if (s=="-s")
 		{
@@ -38,13 +40,37 @@ int main(int argc, char * argv[])
 			//Extract the second file name from the args
 			file_two=string(argv[i]);
 			//subtract
+			break;
 		}
 		if (s=="-i")
 		{
 			i++;
+			//Extract the singular file name from the args
+			file_one=string(argv[i]);
+			//invert
+			break;
+		}
+		if (s=="-l")
+		{
+			i++;
 			//Extract the first file name from the args
 			file_one=string(argv[i]);
-			
+			i++;
+			//Extract the second file name from the args
+			file_two=string(argv[i]);
+			//mask
+			break;
+		}
+		if (s=="-t")
+		{
+			i++;
+			//Extract the singular file name from the args
+			file_one=string(argv[i]);
+			i++;
+			string thresh = string(argv[i]);
+			thresh_int= = atoi(thresh.c_str());
+			//threshold
+			break;
 		}
 		
 	}	
